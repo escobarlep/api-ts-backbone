afacdc2cd2f3c9b531138f6dd459fa68ee17229b
escobarlep <lucasescperes@gmail.com>
2021-02-02 16:18:00 -0300

    test: create bcrypt-adapter

--- src/infra/criptography/bcrypt-adapter.test.ts
+++ src/infra/criptography/bcrypt-adapter.test.ts
@@ -1,11 +1,26 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  hash: async (): Promise<string> => await Promise.resolve('hashed_value')
+}))
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter()
+}
+
 describe('Bcrypt Adapter', () => {
   test('should call bcrypt adapter with correct value', async () => {
-    const sut = new BcryptAdapter()
+    const sut = makeSut()
     const hasher = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hasher).toHaveBeenCalledWith('any_value', sut.salt)
+  })
+
+  test('should call bcrypt adapter with correct value', async () => {
+    const sut = makeSut()
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toEqual('hashed_value')
   })
 })
