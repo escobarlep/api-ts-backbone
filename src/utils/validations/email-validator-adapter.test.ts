f1391da931b206d1433405106791672308e301b5
escobarlep <lucasescperes@gmail.com>
2021-02-02 12:58:48 -0300

    refactor: create method to call Adapter

--- src/utils/validations/email-validator-adapter.test.ts
+++ src/utils/validations/email-validator-adapter.test.ts
@@ -7,22 +7,34 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('Email Validator Adapter', () => {
   test('Should return false when invalid email is provided', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com', false)
     expect(isValid).toBe(false)
   })
   test('Should throw when invalid email is provided', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     expect(() => sut.isValid('valid_email@mail.com'))
       .toThrow(new Error('invalid email'))
   })
   test('Should return true when valid email is provided', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_email@mail.com', false)
     expect(isValid).toBe(true)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    const isEmail = jest.spyOn(validator, 'isEmail')
+
+    sut.isValid('valid_email@mail.com', false)
+    expect(isEmail).toHaveBeenCalledWith('valid_email@mail.com')
   })
 })
